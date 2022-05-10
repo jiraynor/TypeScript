894218082c8381b5687146abaf4f0a444b6aedf2
gondor159 <lacls159@gmail.com>
2022-05-10 19:44:53 +0900

    import 키워드 - import 문 추가

--- modulize/src/index.ts
+++ modulize/src/index.ts
@@ -1,3 +1,6 @@
+// import { 심벌 목록 } from '파일의 상대경로'
+import { IPerson, makePerson } from "./person/Person";
+
 const testMakePerson = (): void => {
   let jane: IPerson = makePerson("Jane");
   let jack: IPerson = makePerson("Jack");
