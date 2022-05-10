9e5eae2a1f2ac523a0d76e68bd8452fda762bf86
gondor159 <lacls159@gmail.com>
2022-05-10 19:59:44 +0900

    외부 패키지를 사용할 때 import 문

--- modulize/src/index.ts
+++ modulize/src/index.ts
@@ -1,6 +1,10 @@
 // import { 심벌 목록 } from '파일의 상대경로'
 import Person, { makePerson } from "./person/Person";
 import IPerson from "./person/IPerson";
+// Chance 클래스 하나만 export default 형태로 제공
+import Chance from "chance";
+// 다양한 기능을 export
+import * as R from "ramda";
 
 const testMakePerson = (): void => {
   let jane: IPerson = makePerson("Jane");
@@ -9,3 +13,9 @@ const testMakePerson = (): void => {
 };
 
 testMakePerson();
+
+const chance = new Chance();
+let persons: IPerson[] = R.range(0, 2).map(
+  (n: number) => new Person(chance.name(), chance.age())
+);
+console.log(persons);
