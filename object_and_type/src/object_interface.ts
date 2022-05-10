b1522260af48cddcb0be1f523c6da8b56c7a3deb
gondor159 <lacls159@gmail.com>
2022-05-10 21:54:03 +0900

    선택 속성 구문

--- object_and_type/src/object_interface.ts
+++ object_and_type/src/object_interface.ts
@@ -14,7 +14,25 @@ interface IPerson {
 }
 let good: IPerson = { name: "Jack", age: 32 };
 
-let bad1: IPerson = { name: "Jack" };
-let bad2: IPerson = { age: 32 };
-let bad3: IPerson = {};
-let bad4: IPerson = { name: "Jack", age: 32, etc: true };
+// age 속성이 없으므로 오류
+// let bad1: IPerson = { name: "Jack" };
+// name 속성이 없으므로 오류
+// let bad2: IPerson = { age: 32 };
+// name과 age 속성이 없으므로 오류
+// let bad3: IPerson = {};
+// etc 속성이 있어서 오류
+// let bad4: IPerson = { name: "Jack", age: 32, etc: true };
+
+/*
+ *  선택 속성 구문
+ */
+
+// 선택 속성 : 필수가 아닌 속성, ? 를 붙여서 만듦
+
+interface IPerson2 {
+  name: string; // 필수 속성
+  age: number; // 필수 속성
+  etc?: boolean; // 선택 속성
+}
+let good1: IPerson2 = { name: "Jack", age: 32 };
+let good2: IPerson2 = { name: "Jack", age: 32, etc: true };
