9a4832cc59ab6f997d64eb8a4117c56fe3f17520
gondor159 <lacls159@gmail.com>
2022-05-11 14:05:00 +0900

    비구조화

--- object_and_type/src/destructing.ts
+++ object_and_type/src/destructing.ts
@@ -27,3 +27,18 @@ let jack: IPerson = { name: 'Jack', age: 32 },
 
 let samsung: ICompany = { name: 'samsung', age: 43 },
   apple: ICompany = { name: 'apple', age: 44 };
+
+/*
+ *  비구조화
+ */
+
+// 구조화된 데이터를 분해하는 것
+
+/*
+ *  비구조화 할당
+ */
+
+// { 속성, 속성 } = 구조화된 객체
+
+let { name, age } = jack;
+console.log(name, age);
