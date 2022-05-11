2094fafbdafdf1251c771b0759f4418711d5d430
gondor159 <lacls159@gmail.com>
2022-05-11 14:42:00 +0900

    타입스크립트 함수 선언

---
+++ function_and_method/src/function_statement.ts
@@ -0,0 +1,13 @@
+/*
+ *  함수 선언문
+ */
+
+// function 키워드와 => 기호로 선언 가능
+
+// function 함수 이름(매개변수1: 타입1, 매개변수2: 타입2[,...]): 반환값 타입 {
+//     함수 몸통
+// }
+
+function add(a: number, b: number): number {
+  return a + b;
+}
